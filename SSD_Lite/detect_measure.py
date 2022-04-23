03b4be54fcdae9299c58d5ef88f9ee34f6b9c661
Yvehming <chang.t.z@qq.com>
2022-04-23 21:04:18 +0800

    update detect_measure.py

--- SSD_Lite/detect_measure.py
+++ SSD_Lite/detect_measure.py
@@ -5,9 +5,7 @@ import numpy as np
 from find_root import detect_root
 import time
 from tflite_runtime.interpreter import Interpreter
-import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
 
 def update_frame():
     frames = pipeline.wait_for_frames()
@@ -37,7 +35,7 @@ if __name__ == "__main__":
     GRAPH_NAME = "pepper_detect_2cat_0.5mnet.tflite"
     LABELMAP_NAME = "pepper_class.txt"
     min_conf_threshold = 0.5
-
+    # uart = uart.uart()
     # 导入模型和类别
     with open(LABELMAP_NAME, 'r') as f:
         labels = [line.strip() for line in f.readlines()]
@@ -51,14 +49,13 @@ if __name__ == "__main__":
     output_details = interpreter.get_output_details()
     height = input_details[0]['shape'][1]
     width = input_details[0]['shape'][2]
-
-    # Loop over every image and perform detection
-    time.sleep(1.0)
+    frame_rate_calc = 1
+    freq = cv2.getTickFrequency()
     # Load image and resize to expected shape [1xHxWx3]
     while True:
-        frames = pipeline.wait_for_frames()
-        # frames.get_depth_frame() is a 640x360 depth image
+        t1 = cv2.getTickCount()
 
+        frames = pipeline.wait_for_frames()
         # RGB图像与深度图像对齐
         aligned_frames = align.process(frames)
 
@@ -85,47 +82,71 @@ if __name__ == "__main__":
         boxes = interpreter.get_tensor(output_details[0]['index'])[0]  # Bounding box coordinates of detected objects
         classes = interpreter.get_tensor(output_details[1]['index'])[0]  # Class index of detected objects
         scores = interpreter.get_tensor(output_details[2]['index'])[0]  # Confidence of detected objects
+        object_name = []
+        detected_boxes = []
         # print(classes)
-
         for i in range(len(scores)):
             if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
                 # Get bounding box coordinates and draw box
                 # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-                if classes[i] == 1:
-                    try:
-                        color_image = update_frame()
-                        frame_show = color_image
-                        root = detect_root()
-                        coordinate = root.find_root(color_image, GRAPH_NAME)
-                        # cv2.circle(root.contour_img, (coordinate[0], coordinate[1]), 10, (0, 0, 0), 1)
-                        cv2.imshow("roi", root.contour_img)
-                        camera_coordinate = np.array(
-                            rs.rs2_deproject_pixel_to_point(depth_intrin, [coordinate[0], coordinate[1]],
-                                                            rs.depth_frame.get_distance(
-                                                                aligned_depth_frame, coordinate[0],
-                                                                coordinate[1])))
-                        roi_z = depth_image[root.xmax - 10:root.ymax + 10, root.xmin:root.ymin + 10]
-                        # pd.DataFrame(roi_z).to_csv('sample.csv')
-                        # f, ax = plt.subplots()
-                        # sns.heatmap(roi_z, center=0, square=True, linewidths=1, cmap=plt.get_cmap('Greens'))
-                        # plt.savefig("heatmap.svg")
-                        # plt.show()
-                        # print(roi_z.shape)
-                        # print(roi_z[root.bottom[1], root.bottom[0]])
-                        print(camera_coordinate * 100)
-                        print("测距结束")
-                        cv2.circle(frame_show, (coordinate[0], coordinate[1]), 10, (225, 0, 0), 1)
-                        cv2.imshow("color_image", frame_show)
-                        key = cv2.waitKey(0)
-                        if key == 27:
-                            cv2.destroyAllWindows()
-                            break
-                    except IndexError:
-                        print("未检测到ROI")
-                        raise
-
-        key = cv2.waitKey(0)
-        if key & 0xFF == ord('q'):
+                ymin = int(max(1, (boxes[i][0] * imH)))
+                xmin = int(max(1, (boxes[i][1] * imW)))
+                ymax = int(min(imH, (boxes[i][2] * imH)))
+                xmax = int(min(imW, (boxes[i][3] * imW)))
+                cv2.rectangle(frame_show, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+                cv2.putText(frame_show, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (255, 255, 0), 2,
+                            cv2.LINE_AA)
+                detected_boxes.append([xmin, ymin, xmax, ymax])
+                object_name.append(labels[int(classes[i])])
+        print(object_name)
+        print(detected_boxes)
+        if 'pepper' in object_name:
+            if cv2.waitKey(1) == ord('d'):
+                try:
+                    color_image = update_frame()
+                    frame_show = color_image
+                    root = detect_root()
+                    coordinate = root.find_root(color_image, GRAPH_NAME)
+                    # cv2.circle(root.contour_img, (coordinate[0], coordinate[1]), 10, (0, 0, 0), 1)
+                    cv2.imshow("roi", root.contour_img)
+                    camera_coordinate = np.array(
+                        rs.rs2_deproject_pixel_to_point(depth_intrin, [coordinate[0], coordinate[1]],
+                                                        rs.depth_frame.get_distance(
+                                                            aligned_depth_frame, coordinate[0],
+                                                            coordinate[1])))
+                    # roi_z = depth_image[root.xmax - 10:root.ymax + 10, root.xmin:root.ymin + 10]
+                    # pd.DataFrame(roi_z).to_csv('sample.csv')
+                    # f, ax = plt.subplots()
+                    # sns.heatmap(roi_z, center=0, square=True, linewidths=1, cmap=plt.get_cmap('Greens'))
+                    # plt.savefig("heatmap.svg")
+                    # plt.show()
+                    # print(roi_z.shape)
+                    # print(roi_z[root.bottom[1], root.bottom[0]])
+                    temp = camera_coordinate.copy()
+                    camera_coordinate[0] = temp[0]
+                    camera_coordinate[1] = temp[2]
+                    camera_coordinate[2] = -temp[1]
+                    
+                    print(camera_coordinate * 100)
+                    print("测距结束")
+                    cv2.circle(frame_show, (coordinate[0], coordinate[1]), 10, (225, 0, 0), 1)
+                    cv2.imshow("color_image", frame_show)
+                    key = cv2.waitKey(0)
+                    if key == 27:
+                        cv2.destroyAllWindows()
+                    if key == ord('q'):
+                        cv2.destroyAllWindows()
+                        break
+                except IndexError:
+                    print("未检测到ROI")
+                    raise
+        cv2.imshow("color_image", frame_show)
+        t2 = cv2.getTickCount()
+        time1 = (t2 - t1) / freq
+        frame_rate_calc = 1 / time1
+        # key = cv2.waitKey(0)
+        if cv2.waitKey(1) == ord('q'):
             cv2.destroyAllWindows()
             break
         # Press any key to continue to next image, or press 'q' to quit
